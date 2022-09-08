7e25d95cd944ade652a3a34ddaf7388b3573c55c
Johnathon Roach <johnathon.roach@gmail.com>
2022-09-08 13:47:29 -0500

    chore: wrap auth in try catch block

--- index.js
+++ index.js
@@ -10,42 +10,47 @@ const userServiceProxy = httpProxy(process.env.PROXY_PATH)
 
 // Authentication
 app.use((req, res, next) => {
-	console.log('Request Headers:', req.headers);
-	let auth = false;
-	if(process.env.MODE === 'key') {
-		console.log('Gateway mode KEY');
-		if(!req.headers.authorization || !process.env.AUTH){
-				return res.json({success: false, message: 'not authorized'});
+	try{
+		console.log('Request Headers:', req.headers);
+		let auth = false;
+		if(process.env.MODE === 'key') {
+			console.log('Gateway mode KEY');
+			if(!req.headers.authorization || !process.env.AUTH){
+					return res.json({success: false, message: 'not authorized'});
+			}
+			auth = bcrypt.compareSync(req.headers.authorization, process.env.AUTH);
 		}
-		auth = bcrypt.compareSync(req.headers.authorization, process.env.AUTH);
+		if(process.env.MODE === 'bless') {
+			console.log('Gateway mode BLESS');
+			const blessed = process.env.BLESSED_URLS.split(',');
+			console.log(`Blessed URLs: ${blessed}`)
+			if(!process.env.BLESSED_URLS){
+					return res.json({success: false, message: 'not authorized'});
+			}
+			const referer = req.headers.referer;
+			const xForwardedFor = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',') : [];
+			console.log(`Referral URL: ${referer}`)
+			console.log(`X-Forwarded-For IPs: ${xForwardedFor}`)
+			auth = blessed.includes(referer);
+
+			if(!auth && xForwardedFor) {
+				console.log('Authorizing X-Forwarded-For', xForwardedFor);
+				auth = xForwardedFor.some(r => blessed.includes(r));
+			}
+		}
+
+		if(auth) {
+			console.log('Authorized');
+			next()
+			return;
+		}
+
+		console.log('Not Authorized');
+		return res.json({success: false, message: 'not authorized'});
+	}catch(err){
+		console.error('An error occurred while authorizing request.', err.message);
+		return res.json({success: false, message: 'not authorized'});
 	}
-	if(process.env.MODE === 'bless') {
-		console.log('Gateway mode BLESS');
-		const blessed = process.env.BLESSED_URLS.split(',');
-		console.log(`Blessed URLs: ${blessed}`)
-		if(!process.env.BLESSED_URLS){
-				return res.json({success: false, message: 'not authorized'});
-		}
-		const referer = req.headers.referer;
-		const xForwardedFor = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',') : [];
-		console.log(`Referral URL: ${referer}`)
-		console.log(`X-Forwarded-For IPs: ${xForwardedFor}`)
-		auth = blessed.includes(referer);
-
-		if(!auth && xForwardedFor) {
-			console.log('X-Forwarded-For Authorized', xForwardedFor);
-			auth = xForwardedFor.some(r => blessed.includes(r));
-		}
-	}
-
-  if(auth) {
-		console.log('Authorized');
-    next()
-    return;
-  }
-
-	console.log('Not Authorized');
-  return res.json({success: false, message: 'not authorized'});
 })
 
 app.post(process.env.PROXY_ROUTE, (req, res, next) => {
