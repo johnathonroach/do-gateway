52f8095c2ad8d74e7c03f926e0d257f1150115b5
Johnathon Roach <johnathon.roach@gmail.com>
2022-10-06 10:55:21 -0500

    format index.js

--- index.js
+++ index.js
@@ -1,66 +1,68 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const dotenv = require('dotenv')
-const httpProxy = require('express-http-proxy')
-const bcrypt = require('bcryptjs');
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const dotenv = require("dotenv");
+const httpProxy = require("express-http-proxy");
+const bcrypt = require("bcryptjs");
 
-dotenv.config()
-const userServiceProxy = httpProxy(process.env.PROXY_PATH)
+dotenv.config();
+const userServiceProxy = httpProxy(process.env.PROXY_PATH);
 
 // Authentication
 app.use((req, res, next) => {
-	try{
-		console.log('Request Headers:', req.headers);
-		console.log('Request URL:', req.url || 'not set');
-		let auth = false;
-		if(process.env.MODE === 'api_key') {
-			console.log('Gateway mode API KEY');
-			if(!req.headers.authorization || !process.env.AUTH){
-					return res.json({success: false, message: 'not authorized'});
-			}
-			auth = bcrypt.compareSync(req.headers.authorization, process.env.AUTH);
-		}
-		if(process.env.MODE === 'header_key') {
-			console.log('Gateway mode HEADER KEY');
-			const key = process.env.HEADER_KEY;
-			const val = process.env.HEADER_VALUE;
-			if(!key || !val){
-					return res.json({success: false, message: 'not authorized'});
-			}
-			auth = req.headers[key] && req.headers[key] === val;
-		}
-		if(process.env.MODE === 'bless') {
-			console.log('Gateway mode BLESS');
-			const blessed = process.env.BLESSED_URLS.split(',');
-			console.log(`Blessed URLs: ${blessed}`)
-			if(!process.env.BLESSED_URLS){
-					return res.json({success: false, message: 'not authorized'});
-			}
-			const referer = req.headers.referer;
-			const xForwardedFor = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',') : [];
-			console.log(`Referral URL: ${referer}`)
-			console.log(`X-Forwarded-For IPs: ${xForwardedFor}`)
-			auth = blessed.includes(referer);
+  try {
+    console.log("Request Headers:", req.headers);
+    console.log("Request URL:", req.url || "not set");
+    let auth = false;
+    if (process.env.MODE === "api_key") {
+      console.log("Gateway mode API KEY");
+      if (!req.headers.authorization || !process.env.AUTH) {
+        return res.json({ success: false, message: "not authorized" });
+      }
+      auth = bcrypt.compareSync(req.headers.authorization, process.env.AUTH);
+    }
+    if (process.env.MODE === "header_key") {
+      console.log("Gateway mode HEADER KEY");
+      const key = process.env.HEADER_KEY;
+      const val = process.env.HEADER_VALUE;
+      if (!key || !val) {
+        return res.json({ success: false, message: "not authorized" });
+      }
+      auth = req.headers[key] && req.headers[key] === val;
+    }
+    if (process.env.MODE === "bless") {
+      console.log("Gateway mode BLESS");
+      const blessed = process.env.BLESSED_URLS.split(",");
+      console.log(`Blessed URLs: ${blessed}`);
+      if (!process.env.BLESSED_URLS) {
+        return res.json({ success: false, message: "not authorized" });
+      }
+      const referer = req.headers.referer;
+      const xForwardedFor = req.headers["x-forwarded-for"]
+        ? req.headers["x-forwarded-for"].split(",")
+        : [];
+      console.log(`Referral URL: ${referer}`);
+      console.log(`X-Forwarded-For IPs: ${xForwardedFor}`);
+      auth = blessed.includes(referer);
 
-			if(!auth && xForwardedFor) {
-				console.log('Authorizing X-Forwarded-For', xForwardedFor);
-				auth = xForwardedFor.some(r => blessed.includes(r));
-			}
-		}
+      if (!auth && xForwardedFor) {
+        console.log("Authorizing X-Forwarded-For", xForwardedFor);
+        auth = xForwardedFor.some((r) => blessed.includes(r));
+      }
+    }
 
-		if(auth) {
-			console.log('Authorized');
-			userServiceProxy(req, res, next);
-			return;
-		}
+    if (auth) {
+      console.log("Authorized");
+      userServiceProxy(req, res, next);
+      return;
+    }
 
-		res.status(401).json({ error: 'Unauthorized' });
-	}catch(err){
-		res.status(401).json({ error: 'Unauthorized' });
-	}
-})
+    res.status(401).json({ error: "Unauthorized" });
+  } catch (err) {
+    res.status(401).json({ error: "Unauthorized" });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
